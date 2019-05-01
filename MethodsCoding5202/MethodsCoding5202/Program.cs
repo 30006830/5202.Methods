abde31ad34d9b9d77fc0a9cd18f30876ed7edd95
Blake Jensen <30006830@student.toiohomai.ac.nz>
2019-05-02 09:28:14 +1200

    1st Method Program

---
+++ MethodsCoding5202/MethodsCoding5202/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsCoding5202
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Explains what the programs funcion is and asks for the user to enter a number.
+            Console.WriteLine("This program is to take a number a display the complete opposite of it (e.g. 10 -> -10)");
+            Console.WriteLine("Please enter a number: ");
+            int num1 = int.Parse(Console.ReadLine());
+            int total = ReturnNumber(num1);
+
+            Console.WriteLine(total);
+            Console.ReadLine();
+        }
+
+        static int ReturnNumber(int number1)
+        {
+            return number1 - number1 * 2;
+        }
+        
+    }
+}
